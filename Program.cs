cbab670a59083850c7976a948f1630f2febcb473
Ana-Costa12 <claudiasantborges@gmail.com>
2021-02-22 13:50:45 -0300

    Teste de comandos 1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Aula_De_PC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Muundo!");
+            console.Writeline("Primeiro teste de comandos")
+        }
+    }
+}
